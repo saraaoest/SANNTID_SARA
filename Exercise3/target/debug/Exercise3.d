d8866f573f227e6ba1fdc56460cc0c43157cbbbe
Sara Ostnor <saraao@stud.ntnu.no>
2025-02-17 09:11:04 +0100

    rust setup exercese3

---
+++ Exercise3/target/debug/Exercise3.d
@@ -0,0 +1,1 @@
+/home/sara/Documents/kyb6/SANNTID_SARA/Exercise3/target/debug/Exercise3: /home/sara/Documents/kyb6/SANNTID_SARA/Exercise3/src/main.rs
---
+++ Exercise3/target/debug/deps/Exercise3-31bdfaadf587ca73.d
@@ -0,0 +1,5 @@
+/home/sara/Documents/kyb6/SANNTID_SARA/Exercise3/target/debug/deps/Exercise3-31bdfaadf587ca73: src/main.rs
+
+/home/sara/Documents/kyb6/SANNTID_SARA/Exercise3/target/debug/deps/Exercise3-31bdfaadf587ca73.d: src/main.rs
+
+src/main.rs:
